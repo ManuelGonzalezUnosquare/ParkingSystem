6c948f61dace2dc08e261838f601005151c00ab3
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-01-29 16:58:57 -0600

    deleted api/dist. docker compose and .env files

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,37 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const dbConfig = {
+          host: config.get<string>("DB_HOST"),
+          port: config.get<number>("DB_PORT"),
+          user: config.get<string>("DB_USER"),
+        };
+        console.log("Tentando conexión a DB con:", dbConfig);
+        return {
+          type: "mysql",
+          host: config.get<string>("DB_HOST"),
+          port: parseInt(config.get<string>("DB_PORT", "3307"), 10),
+          username: config.get<string>("DB_USER"),
+          password: config.get<string>("DB_PASSWORD"),
+          database: config.get<string>("DB_NAME"),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
