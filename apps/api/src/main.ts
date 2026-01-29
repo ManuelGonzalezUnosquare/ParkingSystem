3a923f0761852d5bd00a18bd4826c74013bb1d5c
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-01-29 17:17:51 -0600

    swagger configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"; // Importa Swagger
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,19 @@ async function bootstrap() {
       transform: true,
     })
   );
+
+  // --- Configuración de Swagger ---
+  const config = new DocumentBuilder()
+    .setTitle("Parking System API")
+    .setDescription("The API documentation for the Parking ")
+    .setVersion("1.0")
+    // .addTag("parking")
+    .addBearerAuth() // Para cuando agreguemos JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document); // Se servirá en /api/docs
+  // ------------------------------
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
