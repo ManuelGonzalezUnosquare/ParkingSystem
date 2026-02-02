ecff9b47f4e3486b35f5bf2024f86292ffc86424
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-02-02 12:58:26 -0600

    web: initial installations
    1.- added primeng as UI library and primeicons
    2.- added ngrx/store
    3.- configured primeng

--- apps/parking-portal/src/app/app.config.ts
+++ apps/parking-portal/src/app/app.config.ts
@@ -1,10 +1,34 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from "@angular/core";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { appRoutes } from "./app.routes";
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from "@angular/common/http";
+
+import { providePrimeNG } from "primeng/config";
+import Aura from "@primeuix/themes/aura";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes)
-  ]
+    provideRouter(appRoutes, withComponentInputBinding()),
+    provideHttpClient(withFetch(), withInterceptors([])),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: "none",
+          cssLayer: {
+            name: "primeng",
+            order: "theme, base, primeng",
+          },
+        },
+      },
+    }),
+  ],
 };
