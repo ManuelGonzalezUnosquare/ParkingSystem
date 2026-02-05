27d9fd3fc5fbb0b58b2bb9d48e6fdaf513a33b39
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-02-05 12:06:41 -0600

    libs: user model properties

--- libs/src/lib/shared-models/src/models/user.model.ts
+++ libs/src/lib/shared-models/src/models/user.model.ts
@@ -1,7 +1,10 @@
-import { BaseModel } from "./base.model";
-import { RoleModel } from "./role.model";
+import { BaseModel } from './base.model';
+import { RoleModel } from './role.model';
 
 export interface UserModel extends BaseModel {
+  firstName: string;
+  lastName: string;
+  status: string;
   email: string;
   role: RoleModel;
 }
