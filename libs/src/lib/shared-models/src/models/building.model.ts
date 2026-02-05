db234fdf6ac3ab67277aff03a701fc8f49d0197d
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-02-05 12:11:21 -0600

    libs: building properties

--- libs/src/lib/shared-models/src/models/building.model.ts
+++ libs/src/lib/shared-models/src/models/building.model.ts
@@ -1,6 +1,8 @@
-import { BaseModel } from "./base.model";
+import { BaseModel } from './base.model';
 
 export class BuildingModel extends BaseModel {
   name: string;
   description: string;
+  totalSlots: string;
+  address: string;
 }
