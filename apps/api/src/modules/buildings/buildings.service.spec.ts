6e53dbc1d545a3baf80609dcffaa555a55d486fd
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-02-13 17:01:43 -0600

    test(api): updated and fixed buildings unit tests

--- apps/api/src/modules/buildings/buildings.service.spec.ts
+++ apps/api/src/modules/buildings/buildings.service.spec.ts
@@ -1,34 +1,92 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { BuildingsService } from "./buildings.service";
-import {
-  createMockRepository,
-  MockRepository,
-} from "../../test/mocks/repositories/repository.mock";
-import { Building } from "../../database/entities";
-import { getRepositoryToken } from "@nestjs/typeorm";
+import { Test, TestingModule } from '@nestjs/testing';
+import { BuildingsService } from './buildings.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Building } from '@database/entities';
+import { ConflictException, NotFoundException } from '@nestjs/common';
 
-describe("BuildingsService", () => {
+describe('BuildingsService', () => {
   let service: BuildingsService;
-  let repository: MockRepository<Building>;
+  let repo: any;
+  let testingModule: TestingModule;
+
+  const mockBuilding = {
+    id: 1,
+    publicId: 'b-1',
+    name: 'Tower A',
+    address: '123 St',
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    testingModule = await Test.createTestingModule({
       providers: [
         BuildingsService,
         {
           provide: getRepositoryToken(Building),
-          useValue: createMockRepository(),
+          useValue: {
+            findOneBy: jest.fn(),
+            create: jest.fn(),
+            save: jest.fn(),
+            merge: jest.fn(),
+            softDelete: jest.fn(),
+            createQueryBuilder: jest.fn(() => ({
+              where: jest.fn().mockReturnThis(),
+              getMany: jest.fn(),
+            })),
+          },
         },
       ],
     }).compile();
 
-    service = module.get<BuildingsService>(BuildingsService);
-    repository = module.get<MockRepository<Building>>(
-      getRepositoryToken(Building)
-    );
+    service = testingModule.get<BuildingsService>(BuildingsService);
+    repo = testingModule.get(getRepositoryToken(Building));
   });
 
-  it("should be defined", () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a building if name is unique', async () => {
+      repo.findOneBy.mockResolvedValue(null);
+      repo.create.mockReturnValue(mockBuilding);
+      repo.save.mockResolvedValue(mockBuilding);
+
+      const result = await service.create({
+        name: 'Tower A',
+        address: '123 St',
+        totalSlots: 10,
+      });
+      expect(result).toEqual(mockBuilding);
+    });
+
+    it('should throw ConflictException if name exists', async () => {
+      repo.findOneBy.mockResolvedValue(mockBuilding);
+      await expect(service.create({ name: 'Tower A' } as any)).rejects.toThrow(
+        ConflictException,
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should merge and save updated data', async () => {
+      repo.findOneBy.mockResolvedValue(mockBuilding);
+      repo.merge.mockReturnValue({ ...mockBuilding, name: 'Updated Name' });
+      repo.save.mockResolvedValue({ ...mockBuilding, name: 'Updated Name' });
+
+      const result = await service.update('b-1', { name: 'Updated Name' });
+      expect(repo.merge).toHaveBeenCalled();
+      expect(result.name).toBe('Updated Name');
+    });
+  });
+
+  describe('remove', () => {
+    it('should call softDelete with internal id', async () => {
+      repo.findOneBy.mockResolvedValue(mockBuilding);
+      await service.remove('b-1');
+      expect(repo.softDelete).toHaveBeenCalledWith(mockBuilding.id);
+    });
+
+    it('should throw NotFoundException if building not found', async () => {
+      repo.findOneBy.mockResolvedValue(null);
+      await expect(service.remove('non-existent')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
   });
 });
--- apps/api/src/modules/users/services/users.service.spec.ts
+++ apps/api/src/modules/users/services/users.service.spec.ts
@@ -22,8 +22,6 @@ describe('UsersService', () => {
   let buildingService: BuildingsService;
   let roleService: RoleService;
 
-  // Mock Data
-
   const mockBuilding = {
     id: 1,
     publicId: 'uuid-123',
@@ -47,7 +45,6 @@ describe('UsersService', () => {
     updatedAt: '',
     deletedAt: '',
   };
-
   const mockUser = {
     id: 1,
     email: 'test@test.com',
@@ -55,8 +52,6 @@ describe('UsersService', () => {
     building: { publicId: 'b-1' },
     role: mockRole,
   };
-
-  // Factory para el QueryRunner (El dolor de cabeza de las transacciones)
   const createMockQueryRunner = () => ({
     connect: jest.fn(),
     startTransaction: jest.fn(),
@@ -86,7 +81,6 @@ describe('UsersService', () => {
             softDelete: jest.fn(),
             increment: jest.fn(),
             createQueryBuilder: jest.fn(),
-            // Mock de la conexión para el QueryRunner
             manager: {
               connection: {
                 createQueryRunner: jest.fn().mockReturnValue(queryRunner),
@@ -138,7 +132,6 @@ describe('UsersService', () => {
     };
 
     it('should create a user successfully with transaction', async () => {
-      // Setup Mocks
       repo.exists.mockResolvedValue(false);
       jest
         .spyOn(buildingService, 'findOneByPublicId')
@@ -147,7 +140,7 @@ describe('UsersService', () => {
 
       repo.create.mockReturnValue(mockUser);
       queryRunner.manager.save.mockResolvedValue(mockUser);
-      repo.findOne.mockResolvedValue(mockUser); // Para el findOneByPublicId final
+      repo.findOne.mockResolvedValue(mockUser);
 
       const result = await service.create(createUserDto, mockUser as any);
 
@@ -231,277 +224,3 @@ describe('UsersService', () => {
     });
   });
 });
-
-// import {
-//   ConflictException,
-//   InternalServerErrorException,
-//   NotFoundException,
-// } from '@nestjs/common';
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { getRepositoryToken } from '@nestjs/typeorm';
-//
-// import { UsersService } from './users.service';
-//
-// import { User } from '@database/entities';
-// import { UserStatusEnum } from '@parking-system/libs';
-// import {
-//   MockRepository,
-//   createMockRepository,
-// } from '@test/mocks/repositories/repository.mock';
-// import { createMockCryptoService } from '@test/mocks/services';
-// import { CryptoService } from '@utils/services';
-//
-// describe('UsersService', () => {
-//   let service: UsersService;
-//   let repository: MockRepository<User>;
-//
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [
-//         UsersService,
-//         {
-//           provide: getRepositoryToken(User),
-//           useValue: createMockRepository(),
-//         },
-//         {
-//           provide: CryptoService,
-//           useValue: createMockCryptoService(),
-//         },
-//       ],
-//     }).compile();
-//
-//     service = module.get<UsersService>(UsersService);
-//     repository = module.get<MockRepository<User>>(getRepositoryToken(User));
-//   });
-//
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-//
-//   describe('findAll', () => {
-//     it('should return an array of users', async () => {
-//       const mockUsers = [
-//         { id: 1, email: 'u1@test.com' },
-//         { id: 2, email: 'u2@test.com' },
-//       ];
-//       repository.find.mockResolvedValue(mockUsers);
-//
-//       const result = await service.findAll();
-//
-//       expect(repository.find).toHaveBeenCalled();
-//       expect(result).toEqual(mockUsers);
-//       expect(result.length).toBe(2);
-//     });
-//   });
-//
-//   describe('findOneById', () => {
-//     it('should return a user by numeric ID', async () => {
-//       const mockUser = { id: 1, email: 'test@test.com' };
-//       repository.findOneBy.mockResolvedValue(mockUser);
-//
-//       const result = await service.findOneById(1);
-//
-//       expect(repository.findOneBy).toHaveBeenCalledWith({ id: 1 });
-//       expect(result).toEqual(mockUser);
-//     });
-//
-//     it('should return null if user by ID is not found', async () => {
-//       repository.findOneBy.mockResolvedValue(null);
-//       const result = await service.findOneById(999);
-//       expect(result).toBeNull();
-//     });
-//   });
-//
-//   describe('findOneByPublicId', () => {
-//     it('should return a user if found', async () => {
-//       const mockUser = { id: 1, publicId: 'uuid-123', email: 'test@test.com' };
-//       repository.findOneBy.mockResolvedValue(mockUser);
-//
-//       const result = await service.findOneByPublicId('uuid-123');
-//
-//       expect(repository.findOneBy).toHaveBeenCalledWith({
-//         publicId: 'uuid-123',
-//       });
-//       expect(result).toEqual(mockUser);
-//     });
-//
-//     it('should return null and log warning if user not found', async () => {
-//       repository.findOneBy.mockResolvedValue(null);
-//
-//       const result = await service.findOneByPublicId('non-existent');
-//
-//       expect(result).toBeNull();
-//     });
-//   });
-//
-//   describe('findOneByEmail', () => {
-//     it('should return a user if found', async () => {
-//       const mockUser = { id: 1, publicId: 'uuid-123', email: 'test@test.com' };
-//       repository.findOne.mockResolvedValue(mockUser);
-//
-//       const result = await service.findOneByEmail('test@test.com');
-//
-//       expect(repository.findOne).toHaveBeenCalledWith({
-//         where: { email: 'test@test.com' },
-//         relations: ['role'],
-//       });
-//       expect(result).toEqual(mockUser);
-//     });
-//
-//     it('should return null and log warning if user not found', async () => {
-//       repository.findOne.mockResolvedValue(null);
-//
-//       const result = await service.findOneByEmail('non-existent');
-//
-//       expect(result).toBeNull();
-//     });
-//   });
-//
-//   describe('create', () => {
-//     const createUserDto = {
-//       email: 'new@test.com',
-//       password: 'password123',
-//       firstName: 'John',
-//       lastName: 'Doe',
-//       status: UserStatusEnum.ACTIVE,
-//     };
-//
-//     it('should throw ConflictException if user already exists', async () => {
-//       repository.exists.mockResolvedValue(true);
-//
-//       await expect(service.create(createUserDto)).rejects.toThrow(
-//         ConflictException,
-//       );
-//
-//       expect(repository.save).not.toHaveBeenCalled();
-//     });
-//
-//     it('should successfully create a user if it does not exist', async () => {
-//       repository.exists.mockResolvedValue(false);
-//
-//       const savedUser = { id: 1, ...createUserDto };
-//       repository.create.mockReturnValue(savedUser);
-//       repository.save.mockResolvedValue(savedUser);
-//
-//       const result = await service.create(createUserDto);
-//
-//       expect(repository.exists).toHaveBeenCalledWith({
-//         where: { email: createUserDto.email },
-//       });
-//       expect(result).toEqual(savedUser);
-//       expect(repository.save).toHaveBeenCalled();
-//     });
-//
-//     it('should successfully create a user with a hashed password', async () => {
-//       const rawPassword = 'hashed_password_123';
-//       const hashedPassword = 'hashed_password_abc';
-//       const dtoWithRawPassword = { ...createUserDto, password: rawPassword };
-//
-//       repository.exists.mockResolvedValue(false);
-//       cryptoService.hash.mockResolvedValue(hashedPassword);
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//       repository.create.mockImplementation((data: any) => ({ id: 1, ...data }));
-//       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//       repository.save.mockImplementation((user: any) => Promise.resolve(user));
-//
-//       const result = await service.create(dtoWithRawPassword);
-//
-//       expect(cryptoService.hash).toHaveBeenCalledWith(rawPassword);
-//
-//       expect(repository.create).toHaveBeenCalledWith({
-//         ...dtoWithRawPassword,
-//         password: hashedPassword,
-//       });
-//
-//       expect(result.password).toBe(hashedPassword);
-//     });
-//
-//     it('should throw InternalServerErrorException if save fails unexpectedly', async () => {
-//       repository.exists.mockResolvedValue(false);
-//       repository.create.mockReturnValue(createUserDto);
-//
-//       repository.save.mockRejectedValue(new Error('DB Crash'));
-//
-//       await expect(service.create(createUserDto)).rejects.toThrow(
-//         InternalServerErrorException,
-//       );
-//     });
-//   });
-//
-//   describe('update', () => {
-//     const publicId = 'uuid-123';
-//     const updateDto = { firstName: 'Jane' };
-//     const existingUser = {
-//       id: 1,
-//       publicId,
-//       email: 'test@test.com',
-//       firstName: 'John',
-//     };
-//
-//     it('should successfully update a user', async () => {
-//       repository.findOneBy.mockResolvedValue(existingUser);
-//       const savedUser = { ...existingUser, ...updateDto };
-//       repository.save.mockResolvedValue(savedUser);
-//
-//       const result = await service.update(publicId, updateDto);
-//
-//       expect(repository.save).toHaveBeenCalledWith(
-//         expect.objectContaining(updateDto),
-//       );
-//       expect(result.firstName).toBe('Jane');
-//     });
-//
-//     it('should throw NotFoundException if user to update does not exist', async () => {
-//       repository.findOneBy.mockResolvedValue(null);
-//
-//       await expect(service.update(publicId, updateDto)).rejects.toThrow(
-//         NotFoundException,
-//       );
-//     });
-//
-//     it('should throw InternalServerErrorException if update fails', async () => {
-//       repository.findOneBy.mockResolvedValue(existingUser);
-//       repository.save.mockRejectedValue(new Error('Update failed'));
-//
-//       await expect(service.update(publicId, updateDto)).rejects.toThrow(
-//         InternalServerErrorException,
-//       );
-//     });
-//   });
-//
-//   describe('remove', () => {
-//     const publicId = 'uuid-123';
-//     const existingUser = {
-//       id: 1,
-//       publicId,
-//       email: 'test@test.com',
-//       deletedAt: 'today',
-//     };
-//
-//     it('should successfully remove a user', async () => {
-//       repository.findOneBy.mockResolvedValue(existingUser);
-//       repository.softDelete.mockResolvedValue(existingUser);
-//
-//       await service.remove(publicId);
-//
-//       expect(repository.findOneBy).toHaveBeenCalledWith({ publicId });
-//       expect(repository.softDelete).toHaveBeenCalledWith(existingUser.id);
-//     });
-//
-//     it('should throw NotFoundException if user to remove does not exist', async () => {
-//       repository.findOneBy.mockResolvedValue(null);
-//
-//       await expect(service.remove(publicId)).rejects.toThrow(NotFoundException);
-//       expect(repository.softDelete).not.toHaveBeenCalled();
-//     });
-//
-//     it('should throw InternalServerErrorException if removal fails', async () => {
-//       repository.findOneBy.mockResolvedValue(existingUser);
-//       repository.softDelete.mockRejectedValue(new Error('Delete failed'));
-//
-//       await expect(service.remove(publicId)).rejects.toThrow(
-//         InternalServerErrorException,
-//       );
-//     });
-//   });
-// });
