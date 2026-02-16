72f46db07c38013607fcf28094cbc8aa0da168e3
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-02-16 17:18:28 -0600

    test(web): auth layout UTs

--- apps/web/src/app/app.ts
+++ apps/web/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
   imports: [RouterOutlet, ToastModule],
   selector: 'app-root',
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  template: `
+    <main>
+      <p-toast />
+      <router-outlet />
+    </main>
+  `,
 })
 export class App {
   protected title = 'web';
