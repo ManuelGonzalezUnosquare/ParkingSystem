6d79008b9674488dd10393476795d7064f1a07a3
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-02-17 16:30:12 -0600

    test(web): main layout UTs

--- apps/web/src/app/features/layout/main-layout.ts
+++ apps/web/src/app/features/layout/main-layout.ts
@@ -8,7 +8,7 @@ import {
 import { RouterOutlet } from '@angular/router';
 import { AuthStore } from '@core/stores';
 import { Navbar, ResetPasswordModal, Sidebar } from './components';
-import { DialogService } from 'primeng/dynamicdialog';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-main-layout',
@@ -21,29 +21,38 @@ import { DialogService } from 'primeng/dynamicdialog';
 })
 export class MainLayout {
   readonly authStore = inject(AuthStore);
-  readonly dialogService = inject(DialogService);
+  private readonly dialogService = inject(DialogService);
+  private dialogRef: DynamicDialogRef<ResetPasswordModal> | null;
 
-  protected requiresPasswordChange = computed(() => {
+  protected readonly requiresPasswordChange = computed(() => {
     return this.authStore.user()?.requirePasswordChange ?? false;
   });
 
   constructor() {
     effect(() => {
-      const cRequirePasswordChange = this.requiresPasswordChange();
-      if (cRequirePasswordChange) {
-        this.dialogService.open(ResetPasswordModal, {
-          dismissableMask: false,
-          closeOnEscape: false,
-          closable: false,
-          modal: true,
-          pt: {
-            root: 'border-0 bg-transparent',
-            header: 'hidden',
-            content: 'border-0 bg-transparent p-0',
-            footer: 'border-0 bg-transparent p-0',
-          },
-        });
+      const needsChange = this.requiresPasswordChange();
+
+      if (needsChange && !this.dialogRef) {
+        this.openSecurityModal();
+      } else if (!needsChange && this.dialogRef) {
+        this.dialogRef.close();
+        this.dialogRef = null;
       }
+    });
+  }
+
+  private openSecurityModal(): void {
+    this.dialogRef = this.dialogService.open(ResetPasswordModal, {
+      dismissableMask: false,
+      closeOnEscape: false,
+      closable: false,
+      modal: true,
+      pt: {
+        root: { class: 'border-none shadow-none bg-transparent' },
+        mask: { class: 'backdrop-blur-md bg-slate-900/60' },
+        content: { class: 'p-0 bg-transparent' },
+        header: { class: 'hidden' },
+      },
     });
   }
 }
