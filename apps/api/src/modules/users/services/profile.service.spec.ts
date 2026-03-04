21102a8025a4bee20aa0690e0f9ff316ee9527d0
Manuel Gonzalez - Unosquare <manuel.unosquare@gmail.com>
2026-03-04 12:32:09 -0600

    feat(api): profile service UTs

--- apps/api/src/modules/users/services/profile.service.spec.ts
+++ apps/api/src/modules/users/services/profile.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfileService } from './profile.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '@database/entities';
 
 describe('ProfileService', () => {
   let service: ProfileService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfileService],
+      providers: [
+        ProfileService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<ProfileService>(ProfileService);
